374f281ea99b548a7c9c9db493cb3b8d189cb345
Chandan Kharbanda <chandan.kharbanda5@gmail.com>
2016-05-05 18:42:07 +0530

    add task model

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 var express = require('express');
 
+var model = require('./model');
+
+model.connectDB();
+
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('hello world');
+// console.log(model.Task);
+
+app.get('/tasks', function(req, res) {
+    model.Task.find().exec(function(err, tasks){
+        if(err) {
+            console.log("404");
+            res.statusCode(404);
+            res.end();
+        }
+        if(tasks) {
+            console.log(tasks);
+            res.send(tasks);
+        }
+    });
 });
+
+// var task1 = new model.Task({description: 'task1'});
+// task1.save(function(err){
+//    if(err)
+//        console.log(err);
+//     else
+//        console.log(task1.description);
+// });
 
 app.listen(8080);
