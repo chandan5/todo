c921daec6fb881f6bd9093a40db8e48cf6c95dc9
Chandan Kharbanda <chandan.kharbanda5@gmail.com>
2016-05-06 16:03:16 +0530

    us generators in controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,17 @@
-var model = require('model');
+'use strict';
+const model = require('model');
+const bluebird = require('bluebird');
+
 
 function getAll(req,res) {
-    model.Task.find()
-        .select('description _id')
-        .exec()
-        .then(function (tasks) {
-            res.json(tasks);
-        })
+    bluebird.coroutine(function *() {
+        console.log(1);
+        let tasks = yield model.Task.find()
+            .select('description _id').exec();
+        console.log(2);
+        res.json(tasks);
+        console.log(3);
+    })()
         .catch(function(err) {
             console.log(err);
             res.sendStatus(404);
@@ -15,19 +20,17 @@ function getAll(req,res) {
 
 
 function getById(req, res) {
-    // console.log(req.headers);
-    // console.log(req.params);
     var taskId = req.params.taskId;
-    console.log(taskId);
-    model.Task.findById(taskId)
-        .select('description _id')
-        .exec()
-        .then(function (task) {
-            res.json(task);
-        })
-        .catch(function(err) {
-            console.log(err);
-            res.sendStatus(404);
+    bluebird.coroutine(function *() {
+        let task = yield model.Task.findById(taskId)
+            .select('description _id')
+            .exec();
+        res.json({
+            task: task
+        });
+    })()
+        .catch(function () {
+            res.sendStatus(400);    
         });
 }
 
@@ -36,17 +39,16 @@ function create(req,res) {
     // console.log(req.params);
     // console.log(req);
     console.log(req.body);
-
-    var newTask = new model.Task({
-        description: req.body.description
-    });
-
-    newTask.save()
-        .then(function(task) {
-            res.json({
-                task: task
-            });
-        })
+    bluebird.coroutine(function *() {
+        var newTask = new model.Task({
+            description: req.body.description
+        });
+        var task = yield newTask.save();
+        if(!task) {
+            throw new Error();
+        }
+        res.json(task);
+    })()
         .catch(function(){
             res.sendStatus(500);
         });
@@ -56,13 +58,11 @@ function deleteById(req,res) {
     console.log("del req");
     var taskId = req.params.taskId;
     console.log(taskId);
-    model.Task.findById(taskId)
-        .remove()
-        .exec()
-        .then(function(){
-            console.log("deleted task with taskId: " + taskId);
-            res.end();
-        })
+    bluebird.coroutine(function * () {
+        yield model.Task.findById(taskId).remove().exec();
+        console.log("deleted task with taskId: " + taskId);
+        res.end();
+    })()
         .catch(function(){
             console.log("couldn't find task id:" + taskId);
             res.sendStatus(400);
@@ -73,13 +73,11 @@ function updateDescription(req,res) {
     console.log("update request");
     var taskId = req.params.taskId;
     var description = req.body.description;
-    model.Task.findById(taskId)
-        .update({"description":description})
-        .exec()
-        .then(function(){
-            console.log("updated successfully with desc:" + description);
-            res.sendStatus(200);
-        })
+    bluebird.coroutine(function * () {
+        yield model.Task.findById(taskId).update({"description":description}).exec();
+        console.log("updated successfully with desc:" + description);
+        res.sendStatus(200);
+    })()
         .catch(function () {
             console.log("unable to update:"+description);
             res.statusCode(400);
